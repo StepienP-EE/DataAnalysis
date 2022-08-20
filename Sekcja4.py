b88d243660d7ad9c48e7fc58ce8ea3227f5e6cc2
StepienP-EE <111292439+StepienP-EE@users.noreply.github.com>
2022-08-20 13:02:27 +0200

    v2

--- Sekcja4.py
+++ Sekcja4.py
@@ -3,4 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math as math
 
-if __name__ == 'main':
+weekdays = ["Monday",'Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+weekdaysSeries = pd.Series(weekdays)
+freeDays = [False, False, False, False, False, True, True]
+freeDaysSeries = pd.Series(freeDays)
+holidays = {"New Year" : "2018-01-01", "Epiphany" : "2018-01-06", 'Easter' : '2018-04-01'}
+holidaysSeries = pd.Series(holidays)
+
+
+def printing(name):
+
+    print(name)
+
+
+if __name__ == '__main__':
+    printing(weekdaysSeries)
+    printing(freeDaysSeries)
+    printing(holidaysSeries)
+    holidaysSeries.size
